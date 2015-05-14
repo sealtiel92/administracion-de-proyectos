4151af0dcf0c99894a58c659b798e39e869a4540
Irvingordaz <ijoc@live.com.mx>
2015-05-13 22:38:29 -0500

    Modulo Inicio - Pedidos
    Se agregaron estilos a los módulos de inicio y pedidos, se agregó la
    imagen del logo, todo ello en la carpeta llamada "Modulos Irving"

---
+++ src/Modulos Irving/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bienvenido</title>
+</head>
+<body>
+<center>
+		<h1>Bienvenido</h1>
+		
+		<img src="Desktop\logotipo.jpg" width="500" height="250">
+</center>
+</body>
+</html>
---
+++ src/Modulos Irving/pedido.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+<center>
+<h1>Pedidos</h1>
+<div class="tablaPedidos">
+<link rel="stylesheet" href="css/style.css">
+</div>
+<body>
+
+<table class="tabla"> 
+<tr>
+<td>Cantidad</td>
+<td>Fecha de solicitud</td>
+<td>Fecha de entrega</td>
+<td>Descripción</td>
+</tr>
+
+<tr>
+<td>1</td>
+<td>Jueves</td>
+<td>Lunes</td>
+<td>Poco</td>	
+</tr>
+
+<tr>
+<td>2</td>
+<td>Martes</td>
+<td>Viernes</td>
+<td>Poco</td>	
+</tr>
+
+<tr>
+<td>3</td>
+<td>Viernes</td>
+<td>Domingo</td>
+<td>Mucho</td>	
+</tr>
+</table>
+<?php
+
+
+	if($pedido != null)
+	{
+		echo '<table align="center" cellspacing="20" class="table table-bordered">';
+		echo "<th class='info'>Cantidad</th>";
+		echo "<th class='info'>Fecha de solicitud</th>";
+		echo "<th class='info'>Fecha de entrega</th>";
+		echo "<th class='info'>Descripcion</th>";
+		
+		foreach($pedido as $row)
+		{
+		echo "<tr>";
+		echo '<td>'.$row->cant.'</td>';
+		echo '<td>'.$row->fechaI.'</td>';
+		echo '<td>'.$row->fechaE.'</td>';
+		echo '<td width="600">'.$row->desc.'</td>';
+		echo "</tr>";
+		}
+	}else
+	{
+		echo "<br><br><br><h2>No hay pedidos</h2>";
+	}
+
+?>
+
+</center>
+<footer id="piePagina">
+	<br>COPYRIGHT 2015. TODOS LOS DERECHOS RESERVADOS.</br>
+</footer>
+</body>
+</html>
